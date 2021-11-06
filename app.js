28bd5be42a35aeea0f3ac38129a0122f4bf0f664
CLS <clindforsSejsing@gmail.com>
2021-11-06 08:36:45 +0100

    done navbar

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'strict coding'
 
 function openNav() {
-    document.getElementById("myNavbar").style.display = "inline-block";
+    document.getElementById("myNavbar").style.display = "block";
     document.getElementById("myNavbar").style.width = "100%";
     document.getElementById("myNavbar").style.height = "65rem";
     document.getElementById("close_btn").style.marginRight = "0rem";
